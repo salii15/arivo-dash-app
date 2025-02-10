3357e7cec975a0c1a8e976b612aa63658802af6b
salii15 <ahmetsalih13@gmail.com>
2025-02-10 20:04:27 +0300

    fixing push

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -4,9 +4,8 @@ import { buffer } from 'micro';
 import { supabase } from '@/utils/supabase';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-01-27.acacia',
 });
-
 export const config = {
   api: {
     bodyParser: false,
